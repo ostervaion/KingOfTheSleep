331e5a20f4a49366cc5d30725007193dd3f648fe
Julen <etxebejln@gmail.com>
2026-06-14 22:47:18 +0200

    backend docker improvement

--- frontend/app/src/components/dashboard/Protocols.vue
+++ frontend/app/src/components/dashboard/Protocols.vue
@@ -5,7 +5,9 @@ import { useAuthStore } from '@/stores/auth'
 </script>
 
 <template>
-  <div class="font-inter text-sm text-heading flex-1 min-h-0  bg-(--kots-blocks-color)  p-6 rounded-xl overflow-auto">
+  <div
+    class="font-inter text-sm text-heading flex-1 min-h-0 bg-(--kots-blocks-color) p-6 rounded-xl overflow-auto"
+  >
     Protocols
   </div>
 </template>
