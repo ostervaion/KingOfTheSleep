e115ee0e36202300408b91951ed81b275e55ed00
Ana Meili Gomez Urquijo <anagomez@c3r1s3.42urduliz.com>
2026-07-01 19:35:15 +0200

    key input

--- frontend/app/src/components/dashboard/Lobby.vue
+++ frontend/app/src/components/dashboard/Lobby.vue
@@ -1,10 +1,14 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue'
-import Phaser from 'phaser'
+import * as Phaser from 'phaser'
 
 const gameContainer = ref(null)
 
-let game
+let game = null
+let keyA = null
+let keyS = null
+let keyD = null
+let keyW = null
 
 onMounted(() => {
   game = new Phaser.Game({
@@ -14,15 +18,36 @@ onMounted(() => {
     height: 300,
     backgroundColor: '#222',
     scene: {
-      create() {
-        this.add.text(50, 50, 'Hello Phaser!', {
-          fontSize: '32px',
-          color: '#fff'
-        })
-      }
-    }
+      create: create,
+      update: update,
+    },
   })
 })
+
+// preload: preload
+
+function create() {
+  this.add.text(50, 50, 'Helo Phaser!', {
+    fontSize: '32px',
+    color: '#fff',
+  })
+  keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
+  keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+  keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
+  keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+}
+
+function update() {
+  if (keyA.isDown) {
+    console.log('A key pressed')
+  } else if (keyS.isDown) {
+    console.log('S key pressed')
+  } else if (keyD.isDown) {
+    console.log('D key pressed')
+  } else if (keyW.isDown) {
+    console.log('W key pressed')
+  }
+}
 
 onUnmounted(() => {
   game?.destroy(true)
